9b4a4319247c4e7e4d1cdb529432e93bd277b542
Jason King <jkkphys@gmail.com>
2015-10-02 11:27:07 -0400

    Updated Import_COMSOL.py to handle new output format from COMSOL. Updated tables with new values.

--- Import_COMSOL.py
+++ Import_COMSOL.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Sep 14 14:06:41 2015
-Import_COMSOL.py: Import gradient data from COMSOL output.
+Import_COMSOL.py: Generate lookup tables from COMSOL output.
 @author: jkk3
 """
 
@@ -9,7 +9,10 @@ import numpy as np
 import sys
 
 fname = sys.argv[1]
+
 data = np.loadtxt(fname, skiprows = 8, delimiter = ',')
+#data = np.loadtxt(fname, skiprows = 7, delimiter = ',', dtype = 'string')
 
 np.save(fname.split('.')[0] + '_x.npy', data[:,0]*1E6)
-np.save(fname.split('.')[0] + '.npy', data[:,1:])
+np.save(fname.split('.')[0] + '_c.npy', data[:,1::2])
+np.save(fname.split('.')[0] + '_dc.npy', data[:,2::2])
