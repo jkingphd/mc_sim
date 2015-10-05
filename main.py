8f01ca6cb091e4934446c2e3c51c0db91e0e3126
Jason King <jkkphys@gmail.com>
2015-10-05 15:32:33 -0400

    Updated plotting functions.

--- main.py
+++ main.py
@@ -62,52 +62,65 @@ def calc_v(pos, t, x_table, c_table, dc_table):
         c, dc = int_tables(pos, t, x_table, c_table, dc_table)
     return ((8*v)/(3*np.pi))*np.tanh(((chi*np.pi)/(8*v))*(k/np.power((k+c),2))*dc)
 
-data = np.zeros((t_final,n_trials))
-for i in range(n_trials):
-    # Set start position of bacteria and reset start time
-    data[0,i] = np.random.uniform(a,b)
-    t = 1
-    while(t < t_final):
-        data[t,i] = data[t-1,i] + np.random.normal(0.0, sigma, 1) +\
-        calc_v(data[t-1,i], t-1, x_table, c_table, dc_table)*dt
-#        if wb == 0:
-#            t += 1
-#        elif wb == 1:
-#            if data[t,i] < x1:
-#                data[t,i] = x1 + abs(data[t-1,i] - data[t,i])
-#            else:
-#                t += 1
-#        elif wb == 2:
-#            if data[t,i] < x1:
-#                data[t:,i] = x1
-#                break
-#            else:
-#                t += 1
-#        else:
-        t += 1
-        
-#t = 500
-#fig = plt.figure("Histogram", (9,6))
-#ax = fig.add_subplot(111)
-#n = ax.hist(data[t,:], bins = 100, range = (x_table[0],x_table[-1]))[0]
-#ax.axis([x_table[0],x_table[-1],0,1.5*n.max()])
-#ax.grid(True)
-#ax.axvspan(a, b, color = 'gray', alpha = 0.3, lw = 0)
-#plt.show()
-
-times = np.array([1,2,5,10,20,100,200,500,1000,2000])
-
-for t in range(3600):
-    fig = plt.figure('Test', (9,6))
+def pop_plot(data, thresh, save = False, fname = '0.png'):
+    '''Plot percentage of population in target zone.'''
+    t = np.arange(0,np.shape(data)[0])
+    frac = 100*np.sum(data >= thresh, axis = 1)/np.float(np.shape(data)[1])
+    fig = plt.figure("Population Plot", (9,6))
+    ax = fig.add_subplot(111)
+    #ax.scatter(t, frac, c = 'black', lw = 0, s = 5)
+    ax.plot(t, frac, c = 'red', lw = 1)
+    idx = (np.abs(frac - 75.0)).argmin()
+    ax.hlines(75.0, t[0], t[-1], color = 'black', linestyle = 'dashed')
+    ax.text(idx, 70.0, '$t_{75} = %d$' % idx, fontsize = 16)
+    idx = (np.abs(frac - 50.0)).argmin()
+    ax.hlines(50.0, t[0], t[-1], color = 'black', linestyle = 'dashed')
+    ax.text(idx, 45.0, '$t_{50} = %d$' % idx, fontsize = 16)
+    ax.grid(True)
+    ax.axis([t[0],t[-1],0.0,100.0])
+    ax.set_ylabel("Population (%)", size = 16)
+    ax.set_xlabel("Time (s)", size = 16)
+    ax.set_title("Population vs. Time")
+    # Choose whether or not to save the file.
+    if save == False:
+        plt.show()
+    else:
+        plt.savefig(fname)
+        plt.close()
+    return None
+    
+def k_plot(data, save = False, fname = '0.png'):
+    '''Generate Kymograph of population in time.'''
+    fig = plt.figure("Kymograph", (8,8))
+    ax = fig.add_subplot(111)
+    graph = []
+    for k in np.arange(0,3600,10):
+        graph.append(np.histogram(data[k,:], bins = np.floor(x_table[-1]/100), range = (x_table[0],x_table[-1]))[0])
+    graph = np.array(graph)
+    ax.imshow(graph, cmap = 'hot', interpolation = 'none', aspect = 'auto', extent = (x_table[0],x_table[-1],3600,0))
+    ax.set_xlabel('Position ($\mu m$)', color = 'black', size = 16)
+    ax.set_ylabel('Time (s)', color = 'black', size = 16)
+    # Choose whether or not to save the file.
+    if save == False:
+        plt.show()
+    else:
+        plt.savefig(fname)
+        plt.close()
+    return None
+    
+def hist_plot(data, t, save = False, fname = '0.png'):
+    '''Plot population distribution and concentraction at time t.'''
+    fig = plt.figure('Histogram', (9,6))
     ax1 = fig.add_subplot(111)
     ax1.axvspan(a, b, color = 'black', alpha = 0.1, lw = 0)
     ax1.hist(data[t,:], bins = 100, range = (x_table[0], x_table[-1]), lw = 0, color = 'blue')
     ax1.axis([x_table[0],x_table[-1],0,n_trials/2])
+    ax1.set_xlabel('Position ($\mu m$)', color = 'black', size = 16)
     ax1.set_ylabel('Population', color = 'blue', size = 16)
     ax1.set_title('t = %05d s' % t, size = 16)
     for tl in ax1.get_yticklabels():
         tl.set_color('blue')
-    
+        
     ax2 = ax1.twinx()
     ax2.plot(x_table, c_table[:,t], color = 'red', lw = 1)
     ax2.scatter(x_table, c_table[:,t], color = 'red', s = 10, lw = 0)
@@ -116,29 +129,20 @@ for t in range(3600):
     ax2.set_ylabel('Concentration (mM)', color = 'red', size = 16)
     for t2 in ax2.get_yticklabels():
         t2.set_color('red')
+    # Choose whether or not to save the file.
+    if save == False:
+        plt.show()
+    else:
+        plt.savefig(fname)
+        plt.close()
+    return None
     
-    plt.savefig('t_%d.png' % (t+1))
-    plt.close()
-
-#plt.savefig('%05d.png' % t)
-#plt.close()
-
-##plt.figure("Kymograph", (8,8))
-#graph = []
-#for k in np.arange(0,900,10):
-#    graph.append(np.histogram(data[k,:], bins = 100, range = (x_table[0],x_table[-1]))[0])
-#graph = np.array(graph)
-#np.savetxt('test', graph, delimiter = ',')
-##plt.imshow(graph, cmap = 'brg', interpolation = 'none')
-##plt.show()
-#
-#while(True):
-#    ans = raw_input("Save dataset to file? (Y/N): ")
-#    if ans.upper() == 'Y':
-#        fname = raw_input("Enter filename: ")
-#        np.savetxt(fname, graph, delimiter = ',')
-#        break
-#    elif ans.upper() == 'N':
-#        break
-#    else:
-#        print "Enter Y or N."
+data = np.zeros((t_final,n_trials))
+for i in range(n_trials):
+    # Set start position of bacteria and reset start time
+    data[0,i] = np.random.uniform(a,b)
+    t = 1
+    while(t < t_final):
+        data[t,i] = data[t-1,i] + np.random.normal(0.0, sigma, 1) +\
+        calc_v(data[t-1,i], t-1, x_table, c_table, dc_table)*dt
+        t += 1
